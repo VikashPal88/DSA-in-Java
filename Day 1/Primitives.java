9386875ab04c64e0de36f7ae465fb7ccbcfd4fdc
VikashPal88 <vs700034@gmail.com>
2025-08-01 22:23:00 +0530

    first commit

---
+++ Day 1/Primitives.java
@@ -0,0 +1,11 @@
+public class Primitives {
+    public static void main(String[] args) {
+//        Premitive Data types
+        int rollno = 45;
+        char letter = 'r';
+        float marks = 56.6f;
+        double largeDecimalNumbers = 439034.34324;
+        long largeIntegers = 8734749832475934L;
+        boolean check = false;
+    }
+}
---
+++ Day 1/TypeCasting.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner((System.in));
+//        float num = input.nextFloat();
+//        int num = input.nextInt();
+//        System.out.println(num);
+
+//        int num = (int)(33.34f);
+//        System.out.println(num);
+
+
+        // automatic type promotion in expressions
+//        int a = 257;
+//        byte b =  (byte)(a);  // 257 % 256 = 1
+
+
+//        byte a = 40;
+//        byte b = 50;
+//        byte c = 100;
+//        int d = a * b /c;
+//        System.out.println(d);
+
+
+        byte b = 50;
+
+    }
+}
